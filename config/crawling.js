bc634e8e77d2fa31030b27ca4a32a373ecb0d8fe
jknadan <jknadan@gmail.com>
2022-07-07 16:25:51 +0900

    Add file : crawling.js for data cralwing

--- config/crawling.js
+++ config/crawling.js
@@ -8,11 +8,16 @@ const axios = require('axios');
 const serviceKey =
     encodeURIComponent('fLOlGqt3rEd2+0TiHJVirM3by2QIg839MAx7eJExVkm/pscLlORdmoDfHBH9cORxEdaiq5S4WecBwoi+LCjfXg==');
 
-// const connection = await pool.getConnection((conn)=>conn);
+
+// 임시방편 대기 함수
+function sleep(ms){
+    const wakeUpTime = Date.now() + ms;
+    while(Date.now() < wakeUpTime){}
+}
 
 function getCityCode(){
 
-
+    console.log("getCityCode 함수 시작");
     let url = 'http://apis.data.go.kr/1613000/SuburbsBusInfoService/getCtyCodeList?serviceKey=' +
         serviceKey
         +'&_type=json'
@@ -20,42 +25,90 @@ function getCityCode(){
     axios.get(url).then((result) => {
 
         // 공공데이터 포털에서 받아온 지역 코드
-        const data = result.data.response.body.items.item;
-        console.log(data[0].cityName);
-        console.log(typeof (data));
+        const codeName = result.data.response.body.items.item;
+        console.log(codeName[0].cityName);
+        console.log(typeof (codeName));
 
-        getTerminalName(data);
+        getTerminalName(codeName);
 
     });
 
 }
 
-function getTerminalName(data){
+async function getTerminalName(codeName){
 
-    Object.keys(data).forEach(function(v){
-        // console.log(v);
+    console.log("getTerminalName 시작");
+/*
+
+    const keyArray = Object.keys(codeName)
+    console.log(keyArray);
+
+    const url = keyArray.forEach((v) => {
+        console.log ("반복문1 시작");
         let url2 = 'http://apis.data.go.kr/1613000/SuburbsBusInfoService/getSuberbsBusTrminlList?serviceKey=' +
             serviceKey +
             '&cityCode=' +
-            data[v].cityCode +
+            codeName[v].cityCode +
             '&numOfRows=100' +
             '&_type=json';
         // console.log(data[v].cityCode);
-
-        // console.log(url2);
-
-        axios.get(url2).then((result)=>{
-            console.log(result.data.response.body.items.item);
-        })
+        console.log(url2);
 
     })
+*/
+
+
+    for(let i in codeName){
+        // console.log(codeName[i]);
+        let url2 = 'http://apis.data.go.kr/1613000/SuburbsBusInfoService/getSuberbsBusTrminlList?serviceKey=' +
+            serviceKey +
+            '&cityCode=' +
+            codeName[i].cityCode +
+            '&numOfRows=600' +
+            '&_type=json';
+
+        console.log(url2);
+        await getOpenApi(url2)
+    }
 
 
 
 }
 
+async function getOpenApi(url){
+    await axios.get(url).then((result)=>{
+        const terminalData = result.data.response.body.items.item
+        // console.log(terminalData);
+        sleep(1000);
+
+        for(let i in terminalData){
+            insertDb(terminalData,i);
+            // console.log(i + "% 진행중..");
+        }
+
+
+
+    })
+}
+
+
+async function insertDb(terminalData,i){
+    const connection = await pool.getConnection((conn)=>conn);
+
+    const query = `insert into terminal(cityName,terminalName) values (?,?);`
+
+    const resultRow = await connection.query(query,[terminalData[i].cityName, terminalData[i].terminalNm]);
+
+    connection.release();
+
+    return console.log(i + " ::: " + terminalData[i]);
+}
+
 function init(){
+    console.log("처음시작");
     getCityCode();
+
+    return 0;
 }
 
 init();
